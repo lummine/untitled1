22ed256b329ac43dd28ff3500cdd12278d2a6d64
lummine <82365397+lummine@users.noreply.github.com>
2022-03-29 15:32:11 +1100

    clean and add
    cleaning up more code
    start work on data entry GUI

--- __main__.py
+++ __main__.py
@@ -1,8 +1,7 @@
-import logging
 from calendar import monthrange
-from tkinter import StringVar, Tk
+from tkinter import BOTH, HORIZONTAL, StringVar, Tk
 from datetime import date
-from tkinter.ttk import Combobox, Style, Entry, Button, Label, Frame
+from tkinter.ttk import Combobox, Style, Entry, Button, Label, Frame, Separator
 
 root = Tk()
 style = Style()
@@ -30,10 +29,26 @@ MonthNameDict = {
     12 : 'December'}
 
 
-def getDay(day):
+
+def AddEntry(Day):
+    global year, month
+
+    for i in root.grid_slaves():
+        i.destroy()
+    
+    Label(text=f'{MonthNameDict[month]} {Day}').grid(row=0, column=1, columnspan=2)
+
+    Label(text='title', width=15).grid(row=1, column=0)
+
+
+    Button(text='save', width=30).grid(row=4, column=1)
+    Button(text='back', command=lambda:GetDay(Day), width=30).grid(row=4, column=2)
+
+
+def GetDay(Day):
     global year, month
     try:
-        with open(f'{year},{month},{day}.csv', 'r') as f:
+        with open(f'{year},{month},{Day}.csv', 'r') as f:
             events = f.readlines()
         for event in events:
             print(f'{event}')
@@ -43,44 +58,39 @@ def getDay(day):
     for i in root.grid_slaves():
         i.destroy()
 
-    for i in range(1,10):
-        a = Combobox(width=8)
-        a['values'] = ('event', 'task', 'reminder')
-        a.grid(row=i, column=0)
-        Entry().grid(row=i, column=1)
-        Entry().grid(row=i, column=2)
-
+    Label(text= f'{MonthNameDict[month]} {Day}', anchor='center', width=15).grid(row=0, column=0)
+    Label(text= '', anchor='center', width=105).grid(row=0, column=1, columnspan=3)
     
-    Label(text= f'{MonthNameDict[month]} {day}' ).grid(row=0, column=0, columnspan=3)
-    Button(text="save", command=DaysOfTheMonth).grid(row=10, column=0, columnspan=3)
+    a=1
+
+    for i in range(1,25):
+        Label(text= i, anchor='center', width=15).grid(row=a, column=0)
+        a += 1
+        Separator(orient=HORIZONTAL).grid(row=a, column=0, columnspan=4, sticky='ew')
+        a += 1
+
+    Button(text="close", command=DaysOfTheMonth).grid(row=50, column=0, columnspan=2)
+    Button(text='Data Entry', command=lambda: AddEntry(Day)).grid(row=50, column=2, columnspan=2)
 
 
 def lastMonth():
     global year, month
-    if month in {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}:
-        month = month - 1
-    elif month == 1:
+    if month == 1:
         month = 12
-        year = year - 1
+        year -= 1
     else:
-        logging.error('bad data (lastMonth) L13')
-        return
-    labelDate.set(f'{MonthNameDict[month]} {str(year)}')
+        month -= 1
     DaysOfTheMonth()
     return year, month
 
 
 def nextMonth():
     global year, month
-    if month in {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}:
-        month = month + 1
-    elif month == 12:
+    if month == 12:
         month = 1
-        year = year + 1
+        year += 1
     else:
-        logging.error('bad data (nextMonth) L26')
-        return
-    labelDate.set(f'{MonthNameDict[month]} {str(year)}')
+        month +=1
     DaysOfTheMonth()
     return year, month
 
@@ -110,7 +120,7 @@ def DaysOfTheMonth():
     for j in range(1,7):
         if int(date(year, month, 1).isoweekday()) == j:
             for i in range(0,j):
-                Button(text='', width=15).grid(row=2, column=i, ipady=15)
+                Button(text='', width=15).grid(row=2, column=i, ipady=20)
 
     row = 2
     for i in range(1, monthrange(year, month)[1] + 1):
@@ -120,7 +130,7 @@ def DaysOfTheMonth():
         else:
             column += 1
 
-        Button(text=i, command=lambda day=i: getDay(day), width=15).grid(row=row, column=column, ipady=15)
+        Button(text=i, command=lambda day=i: GetDay(day), width=15).grid(row=row, column=column, ipady=20)
 
     EndBlanksDict = {
         7 : (1,2,3,4,5,6),
@@ -136,14 +146,14 @@ def DaysOfTheMonth():
     for j in range(1,8):
         if int(date(year, month, monthrange(year, month)[1]).isoweekday()) == j:
             for i in EndBlanksDict[j]:
-                Button(text='', width=15).grid(row=row, column=i, ipady=15)
+                Button(text='', width=15).grid(row=row, column=i, ipady=20)
 
 
 def main():
     DaysOfTheMonth()
 
     root.title('Calender')
-    root.geometry('700x400')
+    root.geometry('700x500')
     root.mainloop()
 
 
